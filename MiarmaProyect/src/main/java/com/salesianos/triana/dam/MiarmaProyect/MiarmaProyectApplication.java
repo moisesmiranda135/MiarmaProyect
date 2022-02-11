8af74d11a901fe945c12b4a588d690679857977f
Moisés Miranda Corrales <moisesmiranda135@gmail.com>
2022-02-11 13:57:52 +0100

    se crea el proyecto

---
+++ MiarmaProyect/src/main/java/com/salesianos/triana/dam/MiarmaProyect/MiarmaProyectApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.dam.MiarmaProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiarmaProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiarmaProyectApplication.class, args);
+	}
+
+}
---
+++ MiarmaProyect/src/test/java/com/salesianos/triana/dam/MiarmaProyect/MiarmaProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.dam.MiarmaProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiarmaProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
